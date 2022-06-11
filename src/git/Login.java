96a5ef45bcc348c449c9ae918bf9cfc80193ef88
SONU <SONU@SONU>
2022-06-11 20:47:32 +0530

    first comments

---
+++ src/git/Login.java
@@ -0,0 +1,15 @@
+package git;
+
+public class Login {
+
+	public static void add(int a,int b)
+	{
+		int c=a+b;
+		System.out.println("Addition Is :- "+c);
+	}
+	public static void main(String[] args) {
+		
+		add(22,11);
+		
+	}
+}
